96d6d1778b8dd972946d6499488efc85d0ceca0e
aqkhan <aqk.neo@gmail.com>
2021-09-02 01:48:36 +0500

    Init

--- db/connection/sql.js
+++ db/connection/sql.js
@@ -10,15 +10,6 @@ const connection = mysql.createConnection({
   database: process.env.DB_NAME,
 });
 
-/*
-    CREATE TABLE IF NOT EXISTS `customers` (
-      id int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
-      email varchar(255) NOT NULL,
-      name varchar(255) NOT NULL,
-      active BOOLEAN DEFAULT false
-    ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-  */
-
 // open the MySQL connection
 connection.connect((error) => {
   if (error) throw error;
